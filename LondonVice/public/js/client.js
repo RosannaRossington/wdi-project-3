cd90f69f0600270df14304a46b93924c6433eb86
iamandyleung <andy.leung.kh@googlemail.com>
2016-05-09 17:34:46 +0100

    crimes showing up on google map win

--- LondonVice/public/js/client.js
+++ LondonVice/public/js/client.js
@@ -38,13 +38,35 @@ LondonViceApp.submitForm = function(){
   var method = $(this).attr('method');
   var url    = $(this).attr("action");
   var data   = $(this).serialize();
-  console.log(data)
+  
   return LondonViceApp.ajaxRequest(method, url, data);
 }
 
 LondonViceApp.getUsers = function(){
     return LondonViceApp.ajaxRequest("get", "/users");
 }
+
+LondonViceApp.createMarkerForCrime = function(crime) {
+  var self   = this;
+  var latlng = new google.maps.LatLng(crime.location.latitude, crime.location.longitude);
+  
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: self.map,
+  });
+};
+
+LondonViceApp.loopThroughCrimes = function(data){
+  return $.each(data.crimes, function(i, crime){
+    LondonViceApp.createMarkerForCrime(crime);
+  });
+};
+
+LondonViceApp.getCrimes = function(){
+  var self = this;
+  return LondonViceApp.ajaxRequest("get", "/crimes")
+    .done(self.loopThroughCrimes);;
+};
 
 LondonViceApp.initialize = function(){
   
@@ -53,7 +75,8 @@ LondonViceApp.initialize = function(){
   var mapOptions = {
     zoom: 12,
     center: new google.maps.LatLng(51.506178,-0.088369),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [{"featureType":"road","elementType":"geometry.fill","stylers":[{"lightness":-100}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"lightness":-100},{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"lightness":100}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":100},{"hue":"#006eff"},{"lightness":-19}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"saturation":-100},{"lightness":-16}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"hue":"#2bff00"},{"lightness":-39},{"saturation":8}]},{"featureType":"poi.attraction","elementType":"geometry.fill","stylers":[{"lightness":100},{"saturation":-100}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"saturation":-100},{"lightness":100}]},{"featureType":"poi.government","elementType":"geometry.fill","stylers":[{"lightness":100},{"saturation":-100}]},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"lightness":100},{"saturation":-100}]},{"featureType":"poi.place_of_worship","elementType":"geometry.fill","stylers":[{"lightness":100},{"saturation":-100}]},{"featureType":"poi.school","elementType":"geometry.fill","stylers":[{"saturation":-100},{"lightness":100}]},{"featureType":"poi.sports_complex","elementType":"geometry.fill","stylers":[{"saturation":-100},{"lightness":100}]}]
   };
 
   this.map = new google.maps.Map(this.canvas, mapOptions);
@@ -62,33 +85,6 @@ LondonViceApp.initialize = function(){
   $("form").on("submit", this.submitForm);
   $("#getUsers").on("click", this.getUsers);
 }
-
-LondonViceApp.getCrimes = function(){
-  var self = this;
-  return $.ajax({
-    type: 'GET',
-    url: "/crimes",
-    dataType: 'json',
-  }).done(self.loopThroughCrimes);
-};
-
-
-LondonViceApp.loopThroughCrimes = function(data){
-  return $.each(data.crimes, function(i, crime){
-    LondonViceApp.createMarkerForCrime(crime);
-    console.log(crime);
-  });
-};
-
-LondonViceApp.createMarkerForCrime = function(crime) {
-  var self   = this;
-  var latlng = new google.maps.LatLng(crime.lat, crime.lng);
-  var marker = new google.maps.Marker({
-    position: latlng,
-    map: self.map,
-  });
-};
-
 
 
 $(function(){
