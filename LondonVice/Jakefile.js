6218f81ab42b514c37b0b93dd2c3394181fd4282
paddydeklee <paddydeklee@gmail.com>
2016-05-11 20:07:11 +0100

    circles

--- LondonVice/Jakefile.js
+++ LondonVice/Jakefile.js
@@ -26,11 +26,9 @@ function getCrimes(){
     return rp(url)
     .then(function(data) {
       var data = JSON.parse(data);
-      //console.log(data);
       var i = 0;
       data.forEach(function(crime){
         if (i % 500 === 0) {
-          console.log(crime);
           var outcomeStatus = crime.outcome_status || "";
           var newCrime = {
             category: crime.category,
@@ -53,8 +51,6 @@ function getCrimes(){
 
           Crime.create(newCrime, function(err, crime) {
             if (err) return console.log(err);
-            return console.log(crime);
-            //console.log("Crime was added to the database");
           })
         }
         i++;
--- LondonVice/public/js/map.js
+++ LondonVice/public/js/map.js
@@ -5,6 +5,67 @@ LondonViceApp.map;
 LondonViceApp.categories = [];
 LondonViceApp.markers    = [];
 LondonViceApp.crimes     = [];
+LondonViceApp.activeCircle;
+
+// ON CLICKING A MARKER, USE GOOGLE GEOMETRIES TO RENDER DYNAMIC GRAPH
+// Add a circle with radius 1000
+// var circle = new.google.maps.Circle({
+//   map: map
+//   radius: // set radius here
+//   fillColor:
+// })
+
+// bindTo('center',marker, 'position') circle centre to marker 
+
+// do a loop on each of the markers passing in each marker as an argument
+
+// if the poly.containsLocation(marker.getPosition(), poly, 3?) console.log those markers as crimes?
+
+// (could also do a computeDistanceBetween() function on all of the markers)
+
+
+//before we get into that try getting all of the markers at a given point
+//called inside the build map function
+// LondonViceApp.initializeCircle = function(lat, lng, marker) {
+//   var map = LondonViceApp.map;
+
+//   var markerRadius = new google.maps.Circle({
+//     center: {lat: lat, lng: lng},
+//     map: map,
+//     radius: 1000, // set radius here
+//     fillColor: '#FF0000',
+//     fillOpacity: 0.15
+//   });
+
+  //add an event listener on all markers to remove make the var centerOfCirle = null
+
+  // google.maps.event.addListener(map, 'click', function(event) {
+  //   console.log(google.maps.geometry.poly.containsLocation(event.latLng ///this needs to the be the centre of each marker on a loop if true the marker needs to be console logged as a crime object///, centerOfLondon));
+  // });
+// }
+
+//every time a user clicks on a marker we need to remove any previously drawn circle
+LondonViceApp.removeCircle = function(circle){
+  LondonViceApp.activeCircle = null
+}
+
+LondonViceApp.setMapOnAllCircle = function(map){
+    LondonViceApp.activeCircle.setMap(map);
+}
+
+// Removes the circle from the map, but keeps the .
+LondonViceApp.clearCircle = function() {
+  LondonViceApp.setMapOnAllCircle(null);
+}
+
+// Deletes the circle and removes from 
+LondonViceApp.deleteCircles = function() {
+  LondonViceApp.clearCircle();
+  LondonViceApp.removeCircle();
+  // LondonViceApp.markers = [];
+}
+
+
 
 // Sets the map on all markers in the array.
 LondonViceApp.setMapOnAll = function(map) {
@@ -75,24 +136,24 @@ LondonViceApp.streetView = function (markerLat, markerLng) {
   var pano = document.getElementById('pano');
 
   var panorama = new google.maps.StreetViewPanorama(
-      document.getElementById('pano'), {
-        position: markerLocation,
-        pov: {
-          heading: 34,
-          pitch: 8         
-        },
-        linksControl: false,
-        panControl: false,
-        enableCloseButton: false,
-        addressControl: false,
-        fullscreenControl: false,
-        zoomControl: false
-      });
+    document.getElementById('pano'), {
+      position: markerLocation,
+      pov: {
+        heading: 34,
+        pitch: 8         
+      },
+      linksControl: false,
+      panControl: false,
+      enableCloseButton: false,
+      addressControl: false,
+      fullscreenControl: false,
+      zoomControl: false
+    });
 
   window.setInterval(function() {
-      var pov = panorama.getPov();
-      pov.heading += 0.2;
-      panorama.setPov(pov);
+    var pov = panorama.getPov();
+    pov.heading += 0.2;
+    panorama.setPov(pov);
   }, 10);
 
 }
@@ -101,22 +162,45 @@ LondonViceApp.addInfoForCrime = function(crime, marker){
   var self = this;
 
   google.maps.event.addListener(marker, 'click', function() {
-    var markerLat = crime.location.latitude;
-    var markerLng = crime.location.longitude;
+    var markerRadius;    
+    var map            = LondonViceApp.map;
+    var markerLat      = crime.location.latitude;
+    var markerLng      = crime.location.longitude;
+    var center         = new google.maps.LatLng(markerLat, markerLng)
     var statusCategory = crime.outcomeStatus ? crime.outcomeStatus.category : "Status unknown";
 
-      $("#popup").removeClass("offscreen")
-      $(".crimeTitle").children().remove();
-      $(".streetView").children().remove();
-      $(".crimeTitle").append("<h4>"+crime.category+"</h4>");
-      $(".crimeTitle").append("<h4>"+crime.location.street.name+"</h4>");
-      $(".crimeTitle").append("<h4>"+statusCategory+"</h4>");
-      $(".streetView").append("<p>"+crime.locationType+"</p>");
+    $("#popup").removeClass("offscreen")
+    $(".crimeTitle").children().remove();
+    $(".streetView").children().remove();
+    $(".crimeTitle").append("<h4>"+crime.category+"</h4>");
+    $(".crimeTitle").append("<h4>"+crime.location.street.name+"</h4>");
+    $(".crimeTitle").append("<h4>"+statusCategory+"</h4>");
+    $(".streetView").append("<p>"+crime.locationType+"</p>");
 
     LondonViceApp.streetView(markerLat, markerLng)
 
+    // draw a circle on the clicked crime?
+
+    
+
+
+    
+    var markerRadius = new google.maps.Circle({
+      center: {lat: markerLat, lng: markerLng},
+      map: map,
+      radius: 2000, // set radius here
+      fillColor: '#FF0000',
+      fillOpacity: 0.15
+    })
+    
+    self.activeCircle = markerRadius;
+    markerRadius.bindTo('center',marker,'position');
+    console.log(self.activeCircle);
+    LondonViceApp.deleteCircles();
   });
 };
+
+
 
 LondonViceApp.createMarkerForPlace = function(){
 
@@ -153,7 +237,8 @@ LondonViceApp.createMarkerForPlace = function(){
 
 LondonViceApp.createMarkerForCrime = function(crime) {
   var self    = this;
-
+  var lat     = crime.location.latitude;
+  var lng     = crime.location.longitude;
   var latlng  = new google.maps.LatLng(crime.location.latitude, crime.location.longitude);
 
   var setIcon; 
@@ -183,7 +268,7 @@ LondonViceApp.createMarkerForCrime = function(crime) {
     // origin:     new google.maps.Point(0, 0),   // origin
     // anchor:     new google.maps.Point(0, 0)   // anchor
   }
-        
+
   var marker = new google.maps.Marker({
     position:  latlng,
     map:       self.map,
@@ -193,6 +278,9 @@ LondonViceApp.createMarkerForCrime = function(crime) {
 
   self.markers.push(marker);
   this.addInfoForCrime(crime, marker);
+
+  //draw circle on each marker - now each crime has a radius when the page loads
+  // LondonViceApp.initializeCircle(lat,lng)
 };
 
 LondonViceApp.loopThroughCrimes = function(data){
@@ -200,7 +288,7 @@ LondonViceApp.loopThroughCrimes = function(data){
     var crime = data[i] 
     LondonViceApp.createMarkerForCrime(crime);
   }
-    
+
 };
 
 LondonViceApp.getCrimes = function(){
@@ -227,23 +315,26 @@ LondonViceApp.buildMap = function() {
     center: new google.maps.LatLng(51.506178,-0.088369),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: [{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.highway","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#e2403d"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"transit","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"transit.station","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#242f42"},{"lightness":17}]}],
-      
+
     //styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
     streetViewControl: false
-    };
- 
+  };
+
 
   LondonViceApp.map = new google.maps.Map(this.canvas, mapOptions);
 
   LondonViceApp.getCrimes();
   LondonViceApp.createMarkerForPlace();
   LondonViceApp.showMarkers();
+  // P - to get all the grimes in london, need to define circle - passing in lat and lng (center of the map) and run readCrimes function on these
+  // LondonViceApp.initializeCircle(lat,lng);
 }
 
 $(function(){
   if (LondonViceApp.checkLoggedIn()) {
     LondonViceApp.getTemplate("home", null, "home", LondonViceApp.buildMap);
   }
+
 });
 
 
